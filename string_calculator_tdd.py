139ccd4c18c74231af8d6081c54d3f243fb60ed0
sakshisawnt1 <sakshisawnt22520@gmail.com>
2025-01-22 21:43:26 +0530

    Fix linting issue

--- string_calculator_tdd.py
+++ string_calculator_tdd.py
@@ -1,8 +1,8 @@
+'''Give class calculate sum of numbers from the given string'''
 class StringCalculator:
     def __init__(self):
         self.delimiter = ','
-    
-    
+
     def add(self, numbers: str) -> int:
         #Return 0 if string is empty
         if not numbers:
@@ -16,15 +16,11 @@ class StringCalculator:
             nums = nums.replace(delimiter, ',')
         else:
             nums = nums.replace('\n', ',')
-        
         # Split string and convert into integers
         integers = [int(n) for n in nums.split(',')]
-        
         # neagtive numbers are not allowed
         negatives = [n for n in integers if n < 0]
         if negatives:
             raise ValueError(f"negative numbers not allowed: {','.join(map(str, negatives))}")
-            
-        
         # Return the sum of integers
         return sum(integers)
