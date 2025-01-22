0953478533fcd611d386ec3f8340f0d0bed8a865
sakshisawnt1 <sakshisawnt22520@gmail.com>
2025-01-22 21:28:07 +0530

    Add delimiter condition

--- Test_string_calculator_tdd.py
+++ Test_string_calculator_tdd.py
@@ -5,3 +5,9 @@ from string_calculator_tdd import StringCalculator
 def test_empty_string():
     calc = StringCalculator()
     assert calc.add("") == 0
+    
+# for a custom delimiter.
+def test_custom_delimiter():
+    calc = StringCalculator()
+    assert calc.add("//;\n1;2") == 3
+    assert calc.add("//*\n1*2*3")==6
--- string_calculator_tdd.py
+++ string_calculator_tdd.py
@@ -7,3 +7,18 @@ class StringCalculator:
         #Return 0 if string is empty
         if not numbers:
             return 0
+        # If the input string has a custom delimiter at the start
+        nums = numbers
+        if numbers.startswith('//'):
+            delimiter, nums = numbers[2:].split('\n', 1)
+            # Use the custom delimiter instead of hardcoding a comma
+            nums = nums.replace('\n', delimiter)
+            nums = nums.replace(delimiter, ',')
+        else:
+            nums = nums.replace('\n', ',')
+        
+        # Split string and convert into integers
+        integers = [int(n) for n in nums.split(',')]
+        
+        # Return the sum of integers
+        return sum(integers)
